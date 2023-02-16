21b1a2ad0965d201dfdf37aa166706a8bcaa802d
Даниил <daniilskomarov@yandex.ru>
2023-02-16 11:34:26 +0300

    pa

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class FilmControllerTest {
     @Test
     void validateFilmTest(){
-        final Film film = new Film("");
+        final Film film = new Film();
         film.setDescription("parampam");
         film.setReleaseDate(LocalDate.of(2003,2,15));
         film.setDuration(-100);
